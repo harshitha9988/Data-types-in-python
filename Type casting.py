2c4e6645b84ae001556b4a4bd7f4cee9400dd035
harshitha9988 <harshitha.uppala@outlook.com>
2025-01-09 21:37:27 -0500

    type casting program

---
+++ Type casting.py
@@ -0,0 +1,13 @@
+w=8
+x="day"
+y=5.89
+z=False
+print(type(w))
+print(type(x))
+print(type(y))
+print(type(z))
+print("Data types after type casting are:")
+w=str(w)
+print(type(w))
+y=int(y)
+print(type(y),y)
